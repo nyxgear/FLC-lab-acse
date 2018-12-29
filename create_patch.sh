573e72cbdf8a44117e735d05ef1f726521eb0db6
notswiss <nonsvizzero@gmail.com>
2018-12-29 16:38:25 +0100

    Added create patch script

---
+++ create_patch.sh
@@ -0,0 +1,9 @@
+if [[ $# -eq 0 ]] ; then
+  echo 'Pass file destination for patch'
+  exit 1
+fi
+git add .
+git diff --staged > patches/$1
+git reset .
+git add patches/
+git commit -m "New patch @ $1"
